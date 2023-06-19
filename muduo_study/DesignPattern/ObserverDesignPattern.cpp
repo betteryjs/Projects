a6148dbebf46b6a56147fd7c84eebccb98950747
betteryjs <aanbnbnbnb@gmail.com>
2023-06-19 21:40:03 +0800

    *.cpp

--- muduo_study/DesignPattern/ObserverDesignPattern.cpp
+++ muduo_study/DesignPattern/ObserverDesignPattern.cpp
@@ -17,16 +17,20 @@
 // 订阅者基类
 class IObserver {
 public:
-    virtual ~IObserver(){};
+    virtual ~IObserver() {};
+
     virtual void Update(const std::string &message_from_subject) = 0;
 };
 
 // 发布者基类
 class ISubject {
 public:
-    virtual ~ISubject(){};
+    virtual ~ISubject() {};
+
     virtual void Attach(IObserver *observer) = 0;
+
     virtual void Detach(IObserver *observer) = 0;
+
     virtual void Notify() = 0;
 };
 
@@ -46,25 +50,30 @@ public:
     /**
      * The subscription management methods.
      */
+    // 在监听的 observer 列表添加元素
     void Attach(IObserver *observer) override {
         list_observer_.push_back(observer);
     }
+
+    // 在监听的 observer 列表移除元素
     void Detach(IObserver *observer) override {
         list_observer_.remove(observer);
     }
+
+    // 向 observer 列表中的每一个元素发送通知
     void Notify() override {
-        std::list<IObserver *>::iterator iterator = list_observer_.begin();
-        HowManyObserver();
-        while (iterator != list_observer_.end()) {
-            (*iterator)->Update(message_);
-            ++iterator;
+        for (IObserver *observer: list_observer_) {
+            observer->Update(message_);
         }
+
     }
 
+    // 订阅者创建通知向发布者发布消息
     void CreateMessage(std::string message = "Empty") {
         this->message_ = message;
         Notify();
     }
+
     void HowManyObserver() {
         std::cout << "There are " << list_observer_.size() << " observers in the list.\n";
     }
@@ -90,11 +99,14 @@ private:
 // 订阅者
 class Observer : public IObserver {
 public:
+    // Observer 构造函数传入发布者对象
+    // this->subject_.Attach(this); 开启订阅
     Observer(Subject &subject) : subject_(subject) {
         this->subject_.Attach(this);
         std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
         this->number_ = Observer::static_number_;
     }
+
     virtual ~Observer() {
         std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
     }
@@ -103,24 +115,28 @@ public:
         message_from_subject_ = message_from_subject;
         PrintInfo();
     }
+
+    //  subject_.Detach(this); 关闭订阅
     void RemoveMeFromTheList() {
         subject_.Detach(this);
         std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
     }
+
     void PrintInfo() {
-        std::cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << "\n";
+        std::cout << "Observer \"" << this->number_ << "\": a new message is available --> "
+                  << this->message_from_subject_ << "\n";
     }
 
 private:
-    std::string message_from_subject_;
-    Subject &subject_;
-    static int static_number_;
-    int number_;
+    std::string message_from_subject_; // 收到发布者发来的消息
+    Subject &subject_; // 传入的发布者对象
+    static int static_number_; // 全部的订阅者数量
+    int number_;  // 订阅者唯一的编号
 };
+int Observer::static_number_ = 0; // 全局订阅者数量
 
-int Observer::static_number_ = 0;
 
-void ClientCode() {
+int main() {
     Subject *subject = new Subject;
     Observer *observer1 = new Observer(*subject);
     Observer *observer2 = new Observer(*subject);
@@ -129,6 +145,7 @@ void ClientCode() {
     Observer *observer5;
 
     subject->CreateMessage("Hello World! :D");
+
     observer3->RemoveMeFromTheList();
 
     subject->CreateMessage("The weather is hot today! :p");
@@ -149,9 +166,5 @@ void ClientCode() {
     delete observer2;
     delete observer1;
     delete subject;
-}
-
-int main() {
-    ClientCode();
     return 0;
 }
